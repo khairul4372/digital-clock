8a717f1c532d454ac7cde962c0e908e501d51aae
Khairul Anam <khairul.anam@pantheon.io>
2022-03-25 11:58:02 +0600

    digital clock

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-function digitalClock() {
-  let date = new Date();
+function DigitalClock(){
+  let date = new Date()
 
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  let clockFormat = 'AM';
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let seconds = date.getSeconds()
+  let timeFormat = 'AM'
 
-  if (hours == 0) {
-    hours = 12;
-  } else if (hours > 12) {
-    hours = hours - 12;
-    clockFormat = 'PM';
+  if(hours === 0){
+    hours = 12
   }
+  if (hours === 12) {
+    timeFormat = 'PM'
+  } else if(hours > 12){
+    hours = hours - 12
+    timeFormat = 'PM'
+  }
+  hours = hours < 10 ? '0'+hours : hours
+  minutes = minutes < 10 ? '0'+minutes : minutes
+  seconds = seconds < 10 ? '0'+seconds : seconds
 
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
+  let clock = `${hours}:${minutes}:${seconds}`
+  
+  document.getElementById("time").innerText = clock
+  document.querySelector('small').innerText = timeFormat
 
-  let finalTime = `${hours}:${minutes}:${seconds}`;
-  document.querySelector('#time').innerText = finalTime;
-  document.querySelector('small').innerText = clockFormat;
-
-  setInterval(digitalClock, 1000);
+  setInterval(DigitalClock, 1000)
 }
 
-digitalClock();
+DigitalClock()
